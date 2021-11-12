dbb3513e814c3b1896ab72f9e1a9cb4068b6e3ab
Victor Henrique <victorhenrique455@gmail.com>
2021-11-12 03:23:40 -0300

    pokedex completa

--- javascript/indexAdd.js
+++ javascript/indexAdd.js
@@ -4,9 +4,11 @@ const indexDezena = "#0"
 const indexCentena = "#"
 
 for(let i = 1; i < index.length; i++){
-  if(i >= 10){
-    index[i].innerHTML = indexDezena.concat(i)
+  if(i > 10 && i < 100){
+    index[i].innerHTML = indexDezena.concat(i+1)
+  } else {
+    index[i].innerHTML = indexCentena.concat(i+1)
   }
   index[0].innerHTML = indexUnidade.concat(1)
-  index[i].innerHTML = indexUnidade.concat(i+1)
+  index[99].innerHTML = indexCentena.concat(100)
 }
