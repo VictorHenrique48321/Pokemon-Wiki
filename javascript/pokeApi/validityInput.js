6be445b8f274f329aea0c02269b6bd537f1aa965
Victor Henrique <victorhenrique455@gmail.com>
2022-02-10 19:35:24 -0300

    update

--- javascript/pokeApi/validityInput.js
+++ javascript/pokeApi/validityInput.js
@@ -1,22 +1,25 @@
-import {listaPokemonMovimento} from "./connectApi.js"
-import {resetarPokemons} from "./connectApi.js"
+import {
+  listaPokemonMovimento
+} from "./connectApi.js"
+import {
+  resetarPokemons
+} from "./connectApi.js"
 
 const pokemonNomes = document.querySelectorAll("[data-pokemon]")
 const botao = document.querySelector("[data-botao]")
 
 pokemoTimeLocalStorage()
 
-botao.addEventListener("click",(evento) => {
+botao.addEventListener("click", (evento) => {
   resetarPokemons();
   const array = []
   pokemonNomes.forEach(pokemon => {
     const nome = pokemon.value
     array.push(nome)
-    if(nome.length <= 0){
+    if (nome.length <= 0) {
       console.log("nome invalido")
     } else {
       listaPokemonMovimento(nome.toLowerCase())
-      const pokemonsGuardados = localStorage.setItem("PokemonTime", JSON.stringify(array))
     }
   })
 })
@@ -25,8 +28,12 @@ function pokemoTimeLocalStorage() {
   const buscarPokemons = localStorage.getItem("PokemonTime")
   const pokemonsConvertidos = JSON.parse(buscarPokemons)
 
-  for(let i = 0; i < pokemonNomes.length; i++){
-    pokemonNomes[i].value = pokemonsConvertidos[i].toLowerCase()
-    listaPokemonMovimento(pokemonsConvertidos[i].toLowerCase())
+  if (buscarPokemons !== null) {
+    for (let i = 0; i < pokemonNomes.length; i++) {
+      pokemonNomes[i].value = pokemonsConvertidos[i].toLowerCase()
+      listaPokemonMovimento(pokemonsConvertidos[i].toLowerCase())
+    }
+  } else {
+    return
   }
 }
